431c4ae43dd96bad0a382d0d16ee23cede7c2268
Brett Slatkin <brett@haxor.com>
2023-12-23 20:56:19 -0800

    Adds simple prompt script

---
+++ prompt.py
@@ -0,0 +1,63 @@
+import sys
+
+from gpt4all import GPT4All
+
+
+def do_setup():
+    print('Loading...')
+    model = GPT4All(
+        'orca-2-13b.Q4_0.gguf',
+        model_path='./',
+        allow_download=False)
+    return model
+
+
+STOP_GENERATING = False
+
+
+def end_turn(token_id, token_string):
+    if STOP_GENERATING:
+        return False
+
+    return True
+
+
+def print_response(response_it):
+    for response in response_it:
+        print(response, end='')
+        sys.stdout.flush()
+
+    print()
+
+
+def do_loop(model):
+    print('Prompt for LLM (type ^D to finish input):')
+    prompt = sys.stdin.read()
+    print()
+    print('Response:')
+    print()
+    response_it = model.generate(
+        prompt=prompt,
+        temp=0.9,
+        top_k=100,
+        top_p=0.6,
+        max_tokens=10_000,
+        streaming=True,
+        callback=end_turn)
+    print_response(response_it)
+
+
+def main():
+    global STOP_GENERATING
+    try:
+        model = do_setup()
+        do_loop(model)
+    except KeyboardInterrupt:
+        STOP_GENERATING = True
+        print('Exiting...')
+
+    sys.exit(0)
+
+
+if __name__ == '__main__':
+    main()
